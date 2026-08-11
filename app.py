15873aac324e1b7afc9c97fe89b3b5f7bf072a09
Montaser from Ubuntu <muntaseri@gmail.com>
2026-08-12 02:24:42 +0600

    change file indentation

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, jsonify
-app = Flask(name)
-@app.route("/") def home(): return jsonify({ "status": "success", "message": "Hello from local application exposed via ngrok" })
-if name == "main": app.run(host="0.0.0.0", port=5000)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({ "status": "success", "message": "Hello from local application exposed via ngrok" })
+
+if __name__ == "__main__":
+    app.run(debug=True)

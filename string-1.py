c62b4502aaba2493ec2b9203b066ae7b16376ebe
Montaser from Ubuntu <muntaseri@gmail.com>
2026-08-14 23:37:43 +0600

    sample string function

---
+++ string-1.py
@@ -0,0 +1,14 @@
+name = "Osman Islam"
+name2 = "Montaser Islam"
+
+
+
+print(name)
+print(name2)
+
+print(type(name))
+print(len(name))
+print(name[5])
+print(name[6:11])
+print(name.upper())
+print(name.lower())
